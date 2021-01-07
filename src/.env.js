f7878339a185981f4fd05e768554b5d58e7f1226
James Edward Baldonado II <jamesb@tawk.to>
2021-01-07 21:56:16 +0800

    Update API and domain

--- src/.env.js
+++ src/.env.js
@@ -1,3 +1,3 @@
 export const API_PORT = '4000';
-export const API_URL = `http://localhost:${API_PORT}`;
-export const DOMAIN_TITLE = 'app.com';
+export const API_URL =  process.env.API_URL || `http://localhost:${API_PORT}`;
+export const DOMAIN_TITLE = process.env.DOMAIN_TITLE || 'app.com';
