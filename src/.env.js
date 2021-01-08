dd1cb039922418455c66eb50e238d7b12294db3e
James Edward Baldonado II <jamesb@tawk.to>
2021-01-08 12:45:43 +0800

    Added CI CD

--- src/.env.js
+++ src/.env.js
@@ -1,2 +1,2 @@
-export const API_URL =  process.env.VUE_APP_API_URL;
+export const API_URL =  process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_URL : `${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_PORT}`;
 export const DOMAIN_TITLE = process.env.VUE_APP_DOMAIN_TITLE;
